e865118c3b73b29f4af33b8e07dc64405d448593
raiders032 <nameks@naver.com>
2020-09-06 11:48:16 +0900

    1.1 Loading Screen

---
+++ Loading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>getting weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 100,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
